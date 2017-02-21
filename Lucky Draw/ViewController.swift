4213fa4cc05f965276ad8db5f85da58eec214fa8
Sunny NG <ngsanluk@gmail.com>
2017-02-21 11:18:46 +0800

    Version 1.0 - Finished storyboarding.  Added outlet and action.

--- Lucky Draw/ViewController.swift
+++ Lucky Draw/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // out connecting to lucky number label which's initial value is "?"
+    @IBOutlet weak var label: UILabel!
+   
+    
+    
+    // Action when user tap draw next number
+    @IBAction func drawNextNumber(_ sender: UIButton) {
+        
+        // Use arc4random_uniform c function to generate a random number and convert it to SWIFT Int type
+        let nextNumber = Int(arc4random_uniform(100)+1)
+        
+        // Update the label display to the generated random number
+        label.text = "\(nextNumber)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
